4c8f9ebdf4849acdcc83ee33bd492fa3c6a49033
Link4Electronics <link4electronics@gmail.com>
2026-08-30 06:02:08 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -8,8 +8,6 @@ export OUTPATH=./dist
 export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=https://raw.githubusercontent.com/drhelius/Gearsystem/refs/heads/master/platforms/shared/desktop/mcp/icon.png
-export DEPLOY_GTK=1
-export GTK_DIR=gtk-3.0
 export DEPLOY_OPENGL=1
 
 # Deploy dependencies
@@ -17,3 +15,7 @@ quick-sharun ./AppDir/bin/gearsystem
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
+
+# Test the app for 12 seconds, if the app normally quits before that time
+# then skip this or check if some flag can be passed that makes it stay open
+quick-sharun --simple-test ./dist/*.AppImage
